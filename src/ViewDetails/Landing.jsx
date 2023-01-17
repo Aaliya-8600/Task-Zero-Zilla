60ed9b6379537347d5619484ac829201d21c8fd9
Aaliya-8600 <aaliyababy8600@gmail.com>
2023-01-17 17:53:07 +0530

    Task Complete

---
+++ src/ViewDetails/Landing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductDetail from "./ProductDetail";
+import Produdct from "./Produdct";
+
+const Landing = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <Produdct /> */}
+        <Routes>
+          <Route path="/cardDetails" element={<ProductDetail />} />
+          <Route path="/" element={<Produdct />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default Landing;
